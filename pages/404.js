a81bcc5c9e2375dc2437495f286f22ed99cf3e3c
artemats <artem.matseruk@gmail.com>
2022-06-02 15:42:18 +0300

    feat: 404 page

--- pages/404.js
+++ pages/404.js
@@ -1,15 +1,22 @@
-import Link from 'next/link';
+import Image from 'next/image';
+import banner from '~static/images/404.svg';
 
 
 
 export default function FourOhFour() {
 	return (
 		<>
-			<h1>404</h1>
-			<br/>
-			<Link href="/">
-				<a>Go back home</a>
-			</Link>
+			<div className="pnf">
+				<div className="pnf-box">
+					<div className="pnf-box-banner">
+						<Image src={banner.src} alt="404" layout="fill"/>
+					</div>
+					<div className="pnf-box-description">
+						<p className="pnf-title font-secondary-bold">Sorry</p>
+						<p className="font-bold">The page you are looking for can’t be found.</p>
+					</div>
+				</div>
+			</div>
 		</>
 	)
 }
